e267f561f7513311da354eebc2ea2437edf5cd94
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-01-31 07:39:57 -0500

    Updating mongo file

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, abort
 
 from src.api.mongo import Mongo # Local DB
 #from src.api.aws import *
@@ -11,17 +11,12 @@ port = 27017
 
 mongo = Mongo(host=host, port=port)
 
-@app.route('/')
-def main():
-    # Standard Opening page
-    return '<h1>Erika Backend</h1>'
-
-@app.route('/db')
+@app.route('/api/v1.0/db/')
 def db():
 	# Database Page used for searching for data
     example_query = {"title" : " Merkel protege suggests reducing gas flow through Nord Stream 2 pipeline "}
-    table = mongo.generate_table_html(query=example_query)
-    return table
+    data = mongo.json_data(db='news', collection='reuters', query=example_query)
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True)
--- server/src/api/mongo.py
+++ server/src/api/mongo.py
@@ -1,3 +1,4 @@
+from flask import jsonify
 from pymongo import MongoClient
 
 class Mongo(object):
@@ -9,32 +10,21 @@ class Mongo(object):
     def client(self):
 	    return MongoClient(self.host, self.port)
 
-    def find(self, query):
-        # Find item in mongoDB given a query
+    def json_data(self, db, collection, query):
+        # Return json file of data from mongoDB
         client = self.client()
-        db = client['news']
-        reuters = db['reuters']
-        result = reuters.find_one(query)
-        title = result['title']
-        article = result['article']
-        authors = result['authors']
-        url = result['meta']['url']
-        tags = result['meta']['tags']
+        db = client[db]
+        collection = db[collection]
+        result = collection.find_one(query)
 
-        return title, article, authors, url, tags
+        data = [
+            {
+                'title' : result['title'],
+                'article': result['article'],
+                'authors': result['authors'],
+                'url' : result['meta']['url'],
+                'tags': result['meta']['tags']
+            }
+        ]
 
-    def database_headers(self):
-        # Some html to help verify info. (Once frontend is complete, this function will be deleted)
-        return '<tr> Title </tr> <tr> Article </tr> <tr> Authors </tr> <tr> Url </tr> <tr> Tags </tr>'
-
-    def generate_table_html(self, query):
-
-        # Makes a query to db and returns data in table format
-        
-        title, article, authors, url, tags = self.find(query=query)
-        table_html_open = '<table style="width:100%">'
-        headers = self.database_headers()
-        data = f'<td> {title} </td> <td> {article} </td> <td> {authors} </td> <td> {url} </td> <td> {tags} </td>'
-        table_html_close = '</table>'
-        html = table_html_open + headers + data + table_html_close
-        return html
+        return jsonify({'data' : data})
