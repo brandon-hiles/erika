b8befead8948ea91d4a65a30ef1027336d11f31d
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-02-17 09:33:57 -0500

    Removed Zombie Code from prod

--- ai/python/main.py
+++ ai/python/main.py
@@ -10,37 +10,10 @@ from src.amanda.data_mining.wsj import WallStreetJournal
 from src.amanda.data_mining.nyt import NewYorkTimes
 
 from src.amanda.db.mongo import Mongo
-# Consider adding Name entity recognition into AI section
 
-# Basic Variables
 host = 'localhost'
 port = 27017
-mongo = Mongo(host=host, port=port)
-client = mongo.client()
-db = client['news']
-print(db)
-# Parser Testing
-# parser = SiteMapParser(host=host, port=port)
 
-# Reuters Testing
 reuters = Reuters(host=host, port=port)
-reuters.store_websites(upper_bound=1000)
-
-# Wall Street Journal Testing
-# wsj = WallStreetJournal(host=host, port=port)
-
-# New York Times Testing
-#nyt = NewYorkTimes(host='localhost', port=27017)
-#data = nyt.get_websites()
-
-# Database Testing
-#mongo = Mongo(host=host, port=port)
-
-# Preprocessing Testing
-#from src.amanda.preprocessing.reuters import ReutersPreProcessing
-
-#example_query = {"title" : " Merkel protege suggests reducing gas flow through Nord Stream 2 pipeline "}
-#r_preprocess = ReutersPreProcessing(host=host, port=port, query=example_query)
-#data = r_preprocess.data_retrival(db='news', collection='reuters')
-#data = data['article']
-#preprocessed_data = r_preprocess.preprocess(data=data)
+wsj = WallStreetJournal(host=host, port=port)
+mongo = Mongo(host=host, port=port)
