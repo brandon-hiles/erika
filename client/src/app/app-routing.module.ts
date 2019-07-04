e0ecaa34e4d46f4f71a7e6e9e0589e32817fea1f
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-07-03 20:10:53 -0400

    Re-organizing code structure

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {RegisterComponent} from './register/register.component';
-import {LoginComponent} from './login/login.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import { GetStartedComponent } from './get-started/get-started.component';
+
+// Main Components
+import { HomeComponent } from './main/home/home.component';
+import { GetStartedComponent } from './main/get-started/get-started.component';
+import { RegisterComponent } from './main/register/register.component';
+import { LoginComponent } from './main/login/login.component';
+
+// Admin Components
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { ArticlesComponent } from './admin/articles/articles.component';
+import { LogoutComponent } from './admin/logout/logout.component';
+import { SettingsComponent } from './admin/settings/settings.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'get-started', component: GetStartedComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'dashboard', component: DashboardComponent},
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,23 +5,39 @@ import { HttpClientModule } from  '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { RegisterComponent } from './register/register.component';
-import { GetStartedComponent } from './get-started/get-started.component';
+
+// Main Components
+import { HomeComponent } from './main/home/home.component';
+import { LoginComponent } from './main/login/login.component';
+import { RegisterComponent } from './main/register/register.component';
+import { GetStartedComponent } from './main/get-started/get-started.component';
+
+// Admin Components
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { ArticlesComponent } from './admin/articles/articles.component';
+import { LogoutComponent } from './admin/logout/logout.component';
+import { SettingsComponent } from './admin/settings/settings.component';
+
+// Footer Components
+import { AboutComponent } from './footer/about/about.component';
+import { LegalComponent } from './footer/legal/legal.component';
+import { PrivacyComponent } from './footer/privacy/privacy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    AboutComponent,
     HomeComponent,
     LoginComponent,
-    DashboardComponent,
     RegisterComponent,
     GetStartedComponent,
+    DashboardComponent,
+    ArticlesComponent,
+    LogoutComponent,
+    SettingsComponent,
+    AboutComponent,
+    LegalComponent,
+    PrivacyComponent,
   ],
   imports: [
     BrowserModule,
