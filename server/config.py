91541fc1c3b6e15aa4bb33aac05566b91b26e32e
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-07-08 22:48:36 -0400

    Adding config file

--- server/config.py
+++ server/config.py
@@ -1,11 +1,20 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+from os.path import join, dirname
+from dotenv import load_dotenv
 
+# Grab environment variables
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# Setup SQL Variables
+server = os.environ.get("SQL_SERVER")
+db = os.environ.get("SQL_DATABASE")
+username = os.environ.get("SQL_USERNAME")
+password = os.environ.get("SQL_PASSWORD")
 
 class Config(object):
+    # ...
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    'sqlite:///' + os.path.join(basedir, 'app.db')
+        f"mysql://{username}:{password}@{server}/{db}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-#'mysql://' + 'localhost:3306'
