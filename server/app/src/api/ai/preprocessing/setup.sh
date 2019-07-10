fb2f64e6b43ca12d470bf8d7476b243b9e56948d
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-07-09 21:36:55 -0400

    Mass delete of files

--- server/app/src/api/ai/preprocessing/setup.sh
+++
@@ -1,7 +0,0 @@
-echo "Downloading additional NLTK dependencies"
-
-python3 
-
-import nltk
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
--- server/app/src/api/aws.py
+++
@@ -1,9 +0,0 @@
-
-
-class AWS(object):
-
-    def __init__(self, credentials):
-        self.credentials = credentials
-
-    def DeployEC2Instance(self, instance):
-        pass
